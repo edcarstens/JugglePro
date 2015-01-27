2bc80db1691d68881878de7e3277d151d83874c8
Ed Carstens <edcarstens@yahoo.com>
2015-01-27 08:20:53 -0600

    updated for fixes to get example5 working

--- examples/example5.js
+++ examples/example5.js
@@ -15,7 +15,7 @@ viewer.view = new JPRO.View(viewer);
 // timing
 viewer.basePeriod = 60;
 viewer.clock = new JPRO.Clock(viewer.basePeriod);
-viewer.dwellRatio = 0.25;
+viewer.dwellRatio = 0.5;
 
 // jugglers
 var Adam = new JPRO.Juggler(viewer, 'Adam');
@@ -23,7 +23,7 @@ viewer.jugglers = [Adam];
 
 // routine
 var rhMapAsync = new JPRO.RowHandMapper('rhmA', [[Adam.hands[0],Adam.hands[1]]], [[2,2]], [[2,2]]);
-var casc = new JPRO.Pattern([[ [[0,3]] ]], rhMapAsync, 3);
+var casc = new JPRO.Pattern([[ [[0,3]] ]], rhMapAsync, 1);
 // dynamic pattern modification
 //var casc2boxSeq = new JPRO.Pattern([[ [[0,3]],[[1,2]],[[0,2]] ]], rhMapAsync, 1);
 var casc2boxSeq = new JPRO.ThrowSeq([[ [[0,3]],[[1,2]],[[0,2]] ]], rhMapAsync);
@@ -49,13 +49,18 @@ casc2box.getRoutine = function(parentRoutine, laf) {
     //viewer.clock.basePeriod *= 2;
     return this.routine;
 };
-var rhMapSync = new JPRO.RowHandMapper('rhmS', [[Adam.hands[0]],
-					[Adam.hands[1]]], [[1],[1]], [[2],[1]]);
+var rhms = new JPRO.RowHandMapper('rhmS', [[Adam.hands[0]],
+					   [Adam.hands[1]]], [[1],[1]], [[2],[1]]);
 var box = new JPRO.Pattern([[ [[1,1]], [[0,2]] ],
-			    [ [[1,2]], [[0,1]] ]], rhMapSync, 3);
+			    [ [[1,2]], [[0,1]] ]], rhms, 2);
+var rhms2 = new JPRO.RowHandMapper('rhmS2', [[Adam.hands[0]],[Adam.hands[1]]],
+					[[1],[1]], [[1],[1]]);
 var box2cascSeq = new JPRO.Pattern([[ [[0,3]], [[0,1]]  ],
-				    [ [[1,1]], [[0,3]] ]], rhMapSync, 1);
-viewer.routine = new JPRO.Routine([casc, casc2box, box, box2cascSeq]);
+				    [ [[1,1]], [[0,3]] ]], rhms2, 1);
+var rhma2 = new JPRO.RowHandMapper('rhmA2', [[Adam.hands[0],Adam.hands[1]]],
+				   [[2,2]], [[1,2]]);
+var box2asyncSeq = new JPRO.Pattern([[ [[0,3]], [[0,3]] ]], rhma2, 1);
+viewer.routine = new JPRO.Routine([casc, casc2box, box, box2cascSeq, box2asyncSeq]);
 //viewer.routine = new JPRO.Routine([casc, casc2boxSeq, box]);
 //viewer.routine = new JPRO.Routine([box]);
 viewer.routine.viewer = viewer;
