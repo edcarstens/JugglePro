df05acbe11a35e089fad88ff7469e825c7197c24
Ed Carstens <edcarstens@yahoo.com>
2017-02-12 15:33:40 -0600

    fix example1

--- examples/example1.js
+++ examples/example1.js
@@ -4,12 +4,12 @@
 
 // Initialize viewer
 var viewer = new JPRO.Viewer();
-viewer.basePeriod = 14;
+viewer.basePeriod = 30;
 viewer.dwellRatio = 0.5;
 viewer.setDefaults();
 viewer.routine.patterns[0].mhn = [[ [[0,5]], [[0,5]], [[0,5]] ]];
 viewer.routine.patterns[0].period = 3;
-viewer.routine.patterns[0].props = 6;
+viewer.routine.patterns[0].props = 5;
 //console.log("viewer.hands=" + viewer.hands);
 viewer.init();
 
