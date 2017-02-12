e5d40cdf1d7d71e3b181db5ee29ae1ca5f63fc3b
Ed Carstens <edcarstens@yahoo.com>
2017-02-12 15:12:53 -0600

    changed example1 to 5-ball cascade

--- examples/example1.js
+++ examples/example1.js
@@ -7,7 +7,7 @@ var viewer = new JPRO.Viewer();
 viewer.basePeriod = 14;
 viewer.dwellRatio = 0.5;
 viewer.setDefaults();
-viewer.routine.patterns[0].mhn = [[ [[0,7]], [[0,5]], [[0,6]] ]];
+viewer.routine.patterns[0].mhn = [[ [[0,5]], [[0,5]], [[0,5]] ]];
 viewer.routine.patterns[0].period = 3;
 viewer.routine.patterns[0].props = 6;
 //console.log("viewer.hands=" + viewer.hands);
